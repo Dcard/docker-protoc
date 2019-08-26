d654c67d4f19e40d665b4f58853b2df89b633bce
Rueian <rueiancsie@gmail.com>
2019-08-26 12:14:35 +0800

    fix: ignore mkdir error

--- golang/docker-entrypoint.sh
+++ golang/docker-entrypoint.sh
@@ -4,7 +4,7 @@ if [ -z "$SSH_PRIVATE_KEY" ]
 then
     echo SSH_PRIVATE_KEY is not provided.
 else
-    mkdir /root/.ssh
+    mkdir -p /root/.ssh
     echo "$SSH_PRIVATE_KEY" > /root/.ssh/id_rsa
     chmod 600 /root/.ssh/id_rsa
 fi
