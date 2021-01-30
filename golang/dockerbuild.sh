b0dd82025e334db9d157d2322d092c119bb03db5
Rueian <rueiancsie@gmail.com>
2021-01-31 00:52:22 +0800

    fix: multi-arch build script

--- golang/dockerbuild.sh
+++ golang/dockerbuild.sh
@@ -2,5 +2,13 @@
 
 export DOCKER_BUILDKIT=1
 
-docker buildx build --push --platform=linux/amd64 --build-arg ARCH=x86_64 -t dcard/protoc:3.14.0-golang-1.15.7 .
-docker buildx build --push --platform=linux/arm64 --build-arg ARCH=aarch_64 -t dcard/protoc:3.14.0-golang-1.15.7 .
+BASE="dcard/protoc:3.14.0-golang-1.15.7"
+AMD64="$BASE-amd64"
+ARM64="$BASE-arm64"
+
+docker buildx build --platform=linux/amd64 --build-arg ARCH=x86_64 -t $AMD64 .
+docker buildx build --platform=linux/arm64 --build-arg ARCH=aarch_64 -t $ARM64 .
+docker push $AMD64
+docker push $ARM64
+docker manifest create $BASE $AMD64 $ARM64
+docker manifest push $BASE
