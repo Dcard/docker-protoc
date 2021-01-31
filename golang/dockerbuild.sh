9538b4ba7b68de9b0f115a12c8237fb5b17d149b
Rueian <rueiancsie@gmail.com>
2021-01-31 17:21:54 +0800

    fix: protoc zip

--- golang/dockerbuild.sh
+++ golang/dockerbuild.sh
@@ -2,13 +2,6 @@
 
 export DOCKER_BUILDKIT=1
 
-BASE="dcard/protoc:3.14.0-golang-1.15.7"
-AMD64="$BASE-amd64"
-ARM64="$BASE-arm64"
+TAG="dcard/protoc:3.14.0-golang-1.15.7"
 
-docker buildx build --platform=linux/amd64 --build-arg ARCH=x86_64 -t $AMD64 .
-docker buildx build --platform=linux/arm64 --build-arg ARCH=aarch_64 -t $ARM64 .
-docker push $AMD64
-docker push $ARM64
-docker manifest create $BASE $AMD64 $ARM64
-docker manifest push $BASE
+docker buildx build --push --platform=linux/amd64,linux/arm64 -t $TAG .
