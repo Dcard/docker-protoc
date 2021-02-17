cdb74bec083eece2f7892a19e96f987ab45d1f1f
Rueian <rueiancsie@gmail.com>
2021-02-18 00:45:12 +0800

    feat: upgrade go 1.16.0

--- golang/dockerbuild.sh
+++ golang/dockerbuild.sh
@@ -2,6 +2,6 @@
 
 export DOCKER_BUILDKIT=1
 
-TAG="dcard/protoc:3.14.0-golang-1.15.7"
+TAG="dcard/protoc:3.14.0-golang-1.16.0"
 
 docker buildx build --push --platform=linux/amd64,linux/arm64 -t $TAG .
