a96e7a6a2954ded9f24ec0a2be731117933f640b
Rueian <rueiancsie@gmail.com>
2021-08-21 16:41:32 +0800

    feat: upgrade go 1.17.0

--- golang/dockerbuild.sh
+++ golang/dockerbuild.sh
@@ -2,6 +2,14 @@
 
 export DOCKER_BUILDKIT=1
 
-TAG="dcard/protoc:3.14.0-golang-1.16.0"
+TAG="dcard/protoc:3.17.3-golang-1.17.0"
+
+[[ -f protoc-3.17.3-linux-aarch_64.zip ]] || curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.17.3/protoc-3.17.3-linux-aarch_64.zip
+[[ -f protoc-3.17.3-linux-x86_64.zip ]] || curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.17.3/protoc-3.17.3-linux-x86_64.zip
+mkdir -p linux/arm64 && unzip -o -d linux/arm64 protoc-3.17.3-linux-aarch_64
+mkdir -p linux/amd64 && unzip -o -d linux/amd64 protoc-3.17.3-linux-x86_64
 
 docker buildx build --push --platform=linux/amd64,linux/arm64 -t $TAG .
+
+rm *.zip
+rm -rf linux
